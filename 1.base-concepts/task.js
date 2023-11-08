ae7334b1b89da33a81ee2280d80b4ab7644e2a3d
Irina <irina-kuppieva@yandex.ru>
2023-11-08 19:30:11 +0300

    The first task is done

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,31 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+    let arr = [];
+    let d = b ** 2 - 4 * a * c;
+    
+    if (d === 0) {
+      arr.push(-b / (2 * a));
+    } else if (d > 0) {
+      arr.push((-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a));
+    }
+    return arr;
 }
+console.log(solveEquation());
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  //Процентная ставка в месяц (от 0 до 1)
+  const monthlyInterest = percent / 100 / 12;
+
+  //Расчет тела кредита (сумма кредита - первоначальны взнос)
+  const loanAmount = amount - contribution;
+
+  //Расчет ежемесячного платежа
+  const monthlyPayment = loanAmount * (monthlyInterest + (monthlyInterest / (Math.pow(1 + monthlyInterest, countMonths) - 1)));
+
+  // Рассчет общей суммы выплат
+  const totalPayment = monthlyPayment * countMonths;
+
+  return +totalPayment.toFixed(2);
 }
+console.log(calculateTotalMortgage());
