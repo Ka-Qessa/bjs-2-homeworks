0a125601014bdca143390b9a0a5863d51e8169e5
Irina <irina-kuppieva@yandex.ru>
2023-11-17 18:37:24 +0300

    The fourth task is done

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,36 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if (this.hasOwnProperty('marks')) {
+        this.marks.push(...marks);
+      } else {
+        return 0;
+      }
 }
 
 Student.prototype.getAverage = function () {
-  
+    if(!this.marks || this.marks.length === 0) {
+        return 0;
+      }
+        let sum = 0;
+        let count = this.marks.length;
+    
+      this.marks.map(item => sum = sum + item);
+      this.average = sum / count;
+      return this.average;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
